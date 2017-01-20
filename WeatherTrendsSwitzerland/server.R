8aeb0c87d4a295609a6c4918e004c5fbd6a7ca69
Frederic Auberson <frederic@auberson.net>
2017-01-20 23:14:15 +0100

    Full featured, with forecast

--- WeatherTrendsSwitzerland/server.R
+++ WeatherTrendsSwitzerland/server.R
@@ -5,25 +5,25 @@ library(memoise)
 shinyServer(function(input, output) {
    
   output$plot <- renderGvis({
-    dataframe <- getData(station = input$station)
+    dataframe <- getData(station = input$station, as.numeric(input$order))
     # General Examples: https://cran.r-project.org/web/packages/googleVis/vignettes/googleVis_examples.html
     # Using Roles: https://cran.r-project.org/web/packages/googleVis/vignettes/Using_Roles_via_googleVis.html
     # Intervals: https://developers.google.com/chart/interactive/docs/gallery/intervals
     lineplot <- gvisLineChart(dataframe, "Year", 
-                             c("Temperature","Temperature.interval.1","Temperature.interval.2","Precipitation","Precipitation.interval.1", "Precipitation.interval.2"),
+                             c("Temperature","Temperature.interval.1","Temperature.interval.2","Temperature.certainty","Precipitation","Precipitation.interval.1", "Precipitation.interval.2", "Precipitation.certainty"),
                              options=list(
                              series="[{targetAxisIndex:0, color:'red'},
                                       {targetAxisIndex:1, color:'blue'}]",
                              intervals="{ style:'area' }",
-                             vAxes="[{title:'Temperature', viewWindowMode:'explicit', viewWindow:{min:-15, max:25}, format: 'decimal'}, 
-                                     {title:'Precipitation', viewWindowMode:'explicit', viewWindow:{min:0, max:800}, format: 'decimal'}]",
+                             vAxes="[{title:'Temperature (Degrees Celcius)', viewWindowMode:'explicit', viewWindow:{min:-15, max:25}, format: 'decimal'}, 
+                                     {title:'Precipitation (mm)', viewWindowMode:'explicit', viewWindow:{min:0, max:800}, format: 'decimal'}]",
                              hAxes=paste("[{title:'Date', viewWindowMode:'explicit', viewWindow:{min:",input$years[1],", max:",input$years[2],"}, format: '####'}]"),
                              width=1200, height=800
                            ))
     lineplot
   })
   
-  getDataInternal <- function(station) {
+  getDataInternal <- function(station, predOrder) {
     url <- "http://www.meteoschweiz.admin.ch/product/output/climate-data/homogenous-monthly-data-processing/data/"
     url <- paste(url, "homog_mo_", station, ".txt", sep = "")
     print(paste("Getting data from", url))
@@ -32,19 +32,37 @@ shinyServer(function(input, output) {
     colClasses <- rep("numeric", 4)
     na.strings <- c("NA")
     dataset <- read.fwf(file = url, widths = widths, header = FALSE, col.names = col.names, colClasses=colClasses, na.strings=na.strings, skip = 28)
-    dataset$Date <- as.Date(paste(dataset$Year, dataset$Month, "1"), format = "%Y %m %d")
-    dataset <- dataset[max(c(0,which(!complete.cases(dataset))))+1:nrow(dataset),]
+    dataset <- na.omit(dataset[max(c(0,which(!complete.cases(dataset))))+1:nrow(dataset),])
     
+    dataYears <- data.frame(Year=seq(from=min(dataset$Year), to=max(dataset$Year)))
+
     dataframe <- data.frame(
       aggregate(Temperature~Year, data=dataset, mean),
       aggregate(Temperature~Year, data=dataset, min)[2],
       aggregate(Temperature~Year, data=dataset, max)[2],
+      rep(TRUE,nrow(dataYears)),
       aggregate(Precipitation~Year, data=dataset, mean)[2],
       aggregate(Precipitation~Year, data=dataset, min)[2],
-      aggregate(Precipitation~Year, data=dataset, max)[2]
+      aggregate(Precipitation~Year, data=dataset, max)[2],
+      rep(TRUE,nrow(dataYears))
     )
-    colnames(dataframe)<-c("Year","Temperature","Temperature.interval.1","Temperature.interval.2","Precipitation","Precipitation.interval.1", "Precipitation.interval.2")
-    dataframe
+    colnames(dataframe)<-c("Year","Temperature","Temperature.interval.1","Temperature.interval.2","Temperature.certainty","Precipitation","Precipitation.interval.1", "Precipitation.interval.2", "Precipitation.certainty")
+    
+    predYears <- data.frame(Year=seq(from=max(dataframe$Year)+1, to=2064))
+    predictions <- data.frame(
+      predYears,
+      predict(lm(Temperature~poly(Year,predOrder), data=dataframe), newdata = predYears),
+      predict(lm(Temperature.interval.1~poly(Year,predOrder), data=dataframe), newdata = predYears),
+      predict(lm(Temperature.interval.2~poly(Year,predOrder), data=dataframe), newdata = predYears),
+      rep(FALSE,nrow(predYears)),
+      predict(lm(Precipitation~poly(Year,predOrder), data=dataframe), newdata = predYears),
+      predict(lm(Precipitation.interval.1~poly(Year,predOrder), data=dataframe), newdata = predYears),
+      predict(lm(Precipitation.interval.2~poly(Year,predOrder), data=dataframe), newdata = predYears),
+      rep(FALSE,nrow(predYears))
+    )
+    colnames(predictions)<-colnames(dataframe)
+    
+    rbind(dataframe, predictions)
   }
   
   getData <- memoise(getDataInternal)
--- WeatherTrendsSwitzerland/ui.R
+++ WeatherTrendsSwitzerland/ui.R
@@ -13,18 +13,46 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
+      p(paste("This small demo shows how to mix time series data with forecasted data, and",
+            "visualise it using Google Chart.")),
+      p(paste("It will load historic weather data from the Swiss Office of Meterology's open data page",
+              "(http://www.meteoswiss.admin.ch/home/climate/past/homogenous-monthly-data.html),",
+              "then fit a regression model to the existing",
+              "data to forecast a number of future years. The type of model (i.e. its order)",
+              "can be selected, allowing us to compare the different results of the forecasting",
+              "model (and the main purpose here is educational, the forecasts are not to be",
+              "understood as a serious attempt at forecasting the effects of global warming,",
+              "for example).")),
+      p(paste("Data is available for several weather stations. The weather station can be selected",
+              "from a drop-down list, which will also show which region and height the station is ",
+              "located at. Note in particular the differences in temperature and precipitations",
+              "depending on the station's location.")),
+      p(paste("The years to display in the chart can be selected using the slider. This allows you",
+              "to zoom in and out, and to show additional years of forecasting up to the year 2064",
+              "(a full 200 years after the time series start).")),
+      p(paste("The data displayed is the yearly average temperature and precipitation, with the highest",
+              "and lowest monthly average shown as a shaded area.")),
+      
        tags$head(tags$style("#myplot{height:600px !important;}")),
       
-       selectInput("station", label = h3("Weather Station:"), 
+       selectInput("station", label = "Weather Station:", 
                   choices = choices, 
                   selected = 1),
       
        sliderInput("years",
                    "Years:",
                    min = 1864,
-                   max = 2050,
-                   value = c(1850, 2017), 
-                   sep="")
+                   max = 2064,
+                   value = c(1925, 2025), 
+                   sep=""),
+       
+       radioButtons("order", "Forcasting model:",
+                    c("Linear (1st Order)" = 1,
+                      "Quadratic (2nd Order)" = 2,
+                      "Cubic (3rd Order)" = 3,
+                      "Quartic (4th Order)" = 4,
+                      "Quintic (5th Order)" = 5),
+                    selected = 2)
     ),
     
     # Show a plot of the generated distribution
